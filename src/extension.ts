11e57636166ebc8b41bdb03b622ac55b79ad1855
Louis <louismo28@gmail.com>
2026-02-12 22:38:59 +0200

    #Modifications - Modified extension.ts, added methods to update and metadata on session change and mpris connected/disconnected/metadata change. - Modified global.d.ts, added typing for connected mpris signals. - Modified stylesheet.css, added styling for zt-media-dash-box, zt-media-art-box, zt-media-title-label and zt-media-artist-label. - Modified mpris-service.ts, added method to get the active mpris player. --- ## Additions - Added zyrotec-dash-component.ui.ts

--- src/extension.ts
+++ src/extension.ts
@@ -1,20 +1,112 @@
-import St from 'gi://St';
-import * as Main from 'resource:///org/gnome/shell/ui/main.js';
 import { Extension } from 'resource:///org/gnome/shell/extensions/extension.js';
+import * as Main from 'resource:///org/gnome/shell/ui/main.js';
+import { ZyrotecDashComponent } from './components/zyrotec-dash-component.ui.js';
+import { MPRIS_CHANGED_SIGNALS } from './enums/mpris/mpris-changed-signals.enum.js';
+import { MprisService } from './services/mpris/mpris-service.js';
+import { MprisMetadata } from './types/mpris/mpris-metadata.type.js';
 
 export default class MyExtension extends Extension {
-  private label?: St.Label;
+  private _mprisService?: MprisService;
+  private _zyrotecDashComponent?: ZyrotecDashComponent;
 
-  enable() {
-    this.label = new St.Label({
-      text: "Zyrotec Media Applet"
+  private _signalIds: number[] = [];
+  private _sessionSignalId?: number;
+
+  private _updateMetadata(metadata: MprisMetadata | null): void {
+    if (!metadata) {
+      return;
+    }
+
+    const artUrl = metadata.artUrl ?? "";
+    const title = metadata.title ?? 'Unknown Title';
+    let artist = "";
+
+    if (Array.isArray(metadata.artist)) {
+      artist = metadata.artist.map(a => typeof a === 'object' && 'unpack' in a ? (a as any).unpack() : a).join(', ');
+    } else {
+      if (typeof metadata.artist === 'object' && 'unpack' in (metadata.artist ?? {})) {
+        artist = (metadata.artist as any)?.unpack();
+      } else {
+        artist = (metadata.artist as any);
+      }
+    }
+
+    if (artist === "") {
+      artist = "Unknown Artist";
+    }
+
+    this._zyrotecDashComponent?.setMediaArtUrl(artUrl);
+    this._zyrotecDashComponent?.setMediaTitle(title);
+    this._zyrotecDashComponent?.setMediaArtist(artist);
+  }
+
+  private _syncSession(): void {
+    if (!this._mprisService || !this._zyrotecDashComponent) {
+      return;
+    }
+
+    if (!this._mprisService.isMprisConnected()) {
+      this._zyrotecDashComponent.getComponent().visible = false;
+      this._zyrotecDashComponent.setMediaArtUrl("");
+      this._zyrotecDashComponent.setMediaTitle("");
+      this._zyrotecDashComponent.setMediaArtist("");
+      return;
+    }
+
+    this._zyrotecDashComponent.getComponent().visible = true;
+
+    const metadata = this._mprisService.getMprisMetadata();
+    this._updateMetadata(metadata);
+  }
+
+  private _handleMprisSignals(): void {
+    if (!this._mprisService) {
+      return;
+    }
+
+    const mprisConnectedSignal = this._mprisService.connect(MPRIS_CHANGED_SIGNALS.playerConnected, this._syncSession.bind(this));
+    const mprisDisconnectedSignal = this._mprisService.connect(MPRIS_CHANGED_SIGNALS.playerDisconnected, this._syncSession.bind(this));
+    const mprisMetaDataSignal = this._mprisService.connect(MPRIS_CHANGED_SIGNALS.metadataChanged, (_, args: MprisMetadata) => {
+      this._updateMetadata(args);
     });
 
-    (Main.panel as any)._rightBox.add_child(this.label);
+    this._signalIds = [...this._signalIds, ...[mprisConnectedSignal, mprisDisconnectedSignal, mprisMetaDataSignal]];
+
+    this._sessionSignalId = Main.sessionMode.connect(
+      "updated",
+      this._syncSession.bind(this)
+    );
+  }
+
+  enable() {
+    this._mprisService = new MprisService();
+    this._zyrotecDashComponent = new ZyrotecDashComponent();
+
+    Main.overview.dash._box.add_child(this._zyrotecDashComponent.getComponent());
+
+    this._handleMprisSignals();
+    this._syncSession();
   }
 
   disable() {
-    this.label?.destroy();
-    this.label = undefined;
+    for (const signalId of this._signalIds) {
+      if (!this._mprisService) {
+        continue;
+      }
+
+      this._mprisService.disconnect(signalId);
+    }
+
+    this._signalIds = [];
+
+    if (this._sessionSignalId) {
+      Main.sessionMode.disconnect(this._sessionSignalId);
+    }
+
+    this._zyrotecDashComponent?.destroy();
+    this._mprisService?.destroy();
+
+    this._zyrotecDashComponent = undefined;
+    this._mprisService = undefined;
   }
 }
--- src/global.d.ts
+++ src/global.d.ts
@@ -1,5 +1,28 @@
+import '@girs/cogl-2.0';
 import '@girs/gjs';
 import '@girs/gjs/dom';
 import '@girs/gnome-shell/ambient';
 import '@girs/gnome-shell/extensions/global';
-import '@girs/cogl-2.0';
+
+import type { MPRIS_CHANGED_SIGNALS } from './enums/mpris/mpris-changed-signals.enum.js';
+import type { MprisLoopStatus } from './types/types/mpris-loop-status.type.js';
+import type { MprisMetadata } from './types/types/mpris-metadata.type.js';
+import type { MprisPlaybackStatus } from './types/types/mpris-playback-status.type.js';
+interface MprisSignalMap {
+    [MPRIS_CHANGED_SIGNALS.playerConnected]: [busName: string];
+    [MPRIS_CHANGED_SIGNALS.playerDisconnected]: [busName: string];
+    [MPRIS_CHANGED_SIGNALS.activePlayerChanged]: [busName: string];
+    [MPRIS_CHANGED_SIGNALS.metadataChanged]: [metadata: MprisMetadata];
+    [MPRIS_CHANGED_SIGNALS.playbackStatusChanged]: [status: MprisPlaybackStatus];
+    [MPRIS_CHANGED_SIGNALS.shuffleChanged]: [enabled: boolean];
+    [MPRIS_CHANGED_SIGNALS.loopStatusChanged]: [loop: MprisLoopStatus];
+}
+
+declare module 'resource:///org/gnome/shell/misc/signals.js' {
+    export interface EventEmitter {
+        connect<K extends keyof MprisSignalMap>(
+            signal: K,
+            callback: (source: this, ...args: MprisSignalMap[K]) => void
+        ): number;
+    }
+}
